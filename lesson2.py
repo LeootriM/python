6fa440584c34682a093b41ab0f39292665dc4eb9
Studenti <leotrimsylejmani03@gmail.com>
2024-11-02 11:34:50 +0100

    ..

--- lesson2.py
+++ lesson2.py
@@ -1,1 +1,9 @@
-print('hello')
+fruits = ["apple" , "banana" , "cherry"]
+print(fruits)
+
+
+
+    #Creating a tuple with information about a person
+
+    person = ("Leotrim" , 16 "Programmer")
+    name, age , proffesion = person
