7bbb44f74437514963f361e89ffc89f144049135
LeootriM <148789167+LeootriM@users.noreply.github.com>
2024-12-20 18:18:01 +0100

    Add files via upload

---
+++ error_handaling.py
@@ -0,0 +1,14 @@
+try:
+    result = 10/10
+except:
+    print("This is an error , please chack again")
+
+
+    fruits ={
+        "banana": 2,
+        "apple" : 7,
+        "peach" : 3
+
+    }
+
+    try :
--- index.py
+++ index.py
@@ -1,4 +1,19 @@
-name = "ll"
+age = 16
 
 
-x = 14
+age_to_str = str(age)
+
+print(age_to_str, "of type", type(age_to_str))
+
+print(bool(0))
+print(bool(14))
+
+print(bool(""))
+print(bool("Gentrit"))
+
+a = 5
+b = "10"
+result1  = a * int(b)
+
+print(result1)
+
