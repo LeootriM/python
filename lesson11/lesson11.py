3e050c9d2c4c9975f8cdb81a84b8e7f510ae5c8e
Student <leotrim@gmail.com>
2025-01-25 02:38:44 -0800

    ..

---
+++ lesson11/lesson11.py
@@ -0,0 +1,9 @@
+import datetime
+
+current_time = datetime.datetime.now()
+
+print("year: ", current_time.year)
+print("month: " ,current_time.month)
+print("day: ", current_time.day)
+print("hour: ", current_time.hour)
+print("minute: " ,current_time.minute)
